6e07dc658c93970363c91bc6a01fa561c36a8e52
mescallo-arquitecto <vibethink@colegiounidadpedagogica.edu.co>
2025-12-27 23:32:37 -0500

    fix: update minimal-tiptap link components
    - Update link-bubble-menu.tsx
    - Update link-edit-block.tsx
    - Update link-popover-block.tsx
    - Update measured-container.tsx

--- packages/ui/src/components/extensions/minimal-tiptap/components/bubble-menu/link-bubble-menu.tsx
+++ packages/ui/src/components/extensions/minimal-tiptap/components/bubble-menu/link-bubble-menu.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as React from 'react'
 import type { ShouldShowProps } from '../../types'
 import type { Editor } from '@tiptap/react'
--- packages/ui/src/components/extensions/minimal-tiptap/components/measured-container.tsx
+++ packages/ui/src/components/extensions/minimal-tiptap/components/measured-container.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as React from 'react'
 import { useContainerSize } from '../hooks/use-container-size'
 
